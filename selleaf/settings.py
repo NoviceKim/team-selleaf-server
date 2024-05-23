f6fd1a1e421a7c288396957dd8551114cec99771
NoviceKim <k7679568@naver.com>
2024-05-24 08:58:01 +0900

    240524 settings.py updated

--- selleaf/settings.py
+++ selleaf/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xrbh@fs#!56dnm^+1ld6l5n5t7n)1d@6t(@r3a&2!_x1j8r*$r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['3.36.208.48', '127.0.0.1', 'localhost']
 
@@ -66,7 +66,7 @@ INSTALLED_APPS = [
     'rest_framework'
 ]
 
-SITE_ID = 3
+SITE_ID = 7
 
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_LOGOUT_ON_GET = True
